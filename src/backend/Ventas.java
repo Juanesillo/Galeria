98ab14adec6a995b15b82be04e9cb6528d79202c
j.trivinon@uniandes.edu.co <j.trivinon@uniandes.edu.co>
2024-04-16 08:42:08 -0500

    creacion de clase abstracta venta

--- src/backend/Ventas.java
+++ src/backend/Ventas.java
@@ -1,5 +1,56 @@
 package backend;
 
+import backend.Clientes.Cliente;
+
 public abstract class Ventas{
 
-}
+    protected Cliente cliente;
+    protected String idVenta;
+    protected Inventario inventario;
+
+    public Ventas(Cliente cliente, String idVenta, Inventario inventario){
+
+        this.cliente=cliente;
+        this.idVenta=idVenta;
+        this.inventario=inventario;
+    }
+
+
+    // getters
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+
+    public String getIdVenta() {
+        return idVenta;
+    }
+
+
+    public Inventario getInventario() {
+        return inventario;
+    }
+
+
+
+    
+    //metodos 
+
+    public abstract Integer generarID();//modificar tanto en subasta como en valor Fijo 
+
+   
+
+
+    // Consideración si el valor booleano de la pieza es false => Esta disponible solo para subasta
+    public abstract boolean isDisponible();
+
+    public abstract void venderPieza();
+    
+
+
+
+    }
+
+
+
