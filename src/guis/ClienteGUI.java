08081aef9cb4966ad2fb96fc4731667efd1c6e40
j.trivinon@uniandes.edu.co <j.trivinon@uniandes.edu.co>
2024-04-21 22:28:47 -0500

    ajustes de registro y cliente gui

--- src/guis/ClienteGUI.java
+++ src/guis/ClienteGUI.java
@@ -1,5 +1,8 @@
 package guis;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -12,13 +15,13 @@ public class ClienteGUI extends BaseFrame {
     }
 
     public void addguicomponent(){
-        setSize(500,400);
+        setSize(500,600);
 
         JLabel Titulo= new JLabel("Acciones");
         Titulo.setForeground(Constantes.ColorTexto);
         Titulo.setBounds(0,25,super.getWidth(),25);
         Titulo.setFont(new Font("Dialog",Font.BOLD,32));
-         Titulo.setHorizontalAlignment(SwingConstants.CENTER);
+        Titulo.setHorizontalAlignment(SwingConstants.CENTER);
         add(Titulo);
 
 
@@ -27,29 +30,58 @@ public class ClienteGUI extends BaseFrame {
         comprar.setBackground(Constantes.SPColor);
         comprar.setBounds(0,120,super.getWidth(),50);
         comprar.setFont(new Font("Dialog",Font.BOLD,20));
+
+
+        // acciones implementadas por consola ya que no se como mostrarlas por gui 
+
+        comprar.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+                // conocer disponibles 
+
+                // conectar con cajero
+
+                    // comprar 
+                // registrar en historial de compras del cliente
+
+            }
+            
+        });
+
+
+
+
         add(comprar);
 
 
         JButton vender= new JButton("VENDER");
         vender.setForeground(Constantes.ColorTexto);
         vender.setBackground(Constantes.SPColor);
-        vender.setBounds(0,200,super.getWidth(),50);
+        vender.setBounds(0,240,super.getWidth(),50);
         vender.setFont(new Font("Dialog",Font.BOLD,20));
 
         add(vender);
 
 
 
-        JButton Subastar= new JButton("SUBASTA");
-        Subastar.setBackground(Constantes.SPColor);
-        Subastar.setForeground(Constantes.ColorTexto);
-        Subastar.setBounds(0, 280, super.getWidth(),50 );
-        Subastar.setFont(new Font("Dialog",Font.BOLD,20));
+        JButton HistorialP= new JButton("Consultar Historial Propiedad");
+        HistorialP.setBackground(Constantes.SPColor);
+        HistorialP.setForeground(Constantes.ColorTexto);
+        HistorialP.setBounds(0, 360, super.getWidth(),50 );
+        HistorialP.setFont(new Font("Dialog",Font.BOLD,20));
 
-        add(Subastar);
+        add(HistorialP);
 
 
+        JButton HitorialCompras= new JButton("Consultar Historial Compras");
+        HitorialCompras.setBackground(Constantes.SPColor);
+        HitorialCompras.setForeground(Constantes.ColorTexto);
+        HitorialCompras.setBounds(0, 480, super.getWidth(),50 );
+        HitorialCompras.setFont(new Font("Dialog",Font.BOLD,20));
 
+        add(HitorialCompras);
         
     }
 
--- src/guis/RegisterGui.java
+++ src/guis/RegisterGui.java
@@ -11,8 +11,7 @@ import java.awt.event.ActionListener;
 
 import java.util.UUID;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
